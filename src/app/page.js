9d80f6ac28fe7c4dc6d7ebf0548d17095ad26097
viunow <vini.bracht1@gmail.com>
2025-04-01 21:52:11 -0300

    fix vertical scroll in mobile devices

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,9 @@ const playfair = Playfair_Display({
 
 export default function Home() {
   return (
-    <div className="bg-blue300 w-full min-h-screen flex flex-col items-center relative">
+    <div className="bg-blue300 w-full h-dvh flex flex-col items-center justify-between overflow-hidden relative">
       <div
-        className={`${playfair.className} text-center max-w-3xl px-4 md:px-0 z-10 mt-10 md:mt-16`}
+        className={`${playfair.className} text-center max-w-3xl px-4 md:px-0 z-10 pt-10 md:pt-16`}
       >
         <h1 className="text-white text-3xl md:text-4xl font-semibold tracking-wide mb-3">
           Em breve, <span className="italic">Celestial Pet</span>
@@ -22,15 +22,19 @@ export default function Home() {
           respeito.
         </p>
       </div>
-      <div className="mt-auto ml-auto">
-        <Image
-          src={cover.src}
-          width={900}
-          height={600}
-          quality={100}
-          alt="Banner Celestial Pet"
-          className="max-w-full h-auto"
-        />
+
+      <div className="w-full h-auto flex justify-end items-end">
+        <div className="relative w-full md:w-auto max-h-[100vh]">
+          <Image
+            src={cover.src}
+            width={900}
+            height={600}
+            quality={100}
+            alt="Banner Celestial Pet"
+            className="w-full h-auto object-contain"
+            priority
+          />
+        </div>
       </div>
     </div>
   );
