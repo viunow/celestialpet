5774ae789a1ea2922d08419e04a5fbaa94518e55
viunow <vini.bracht1@gmail.com>
2025-05-12 01:06:05 -0300

    fixed timestamp

--- src/app/components/VisitorTracker.jsx
+++ src/app/components/VisitorTracker.jsx
@@ -128,7 +128,16 @@ const VisitorTracker = () => {
             ip,
             location: locationData,
             browserInfo,
-            timestamp: new Date().toISOString(),
+            timestamp: new Date(timestamp).toLocaleString("pt-BR", {
+              timeZone: "America/Sao_Paulo",
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+              hour12: false,
+            }),
           }),
         });
 
